8be6facd960f28a2719d549e3847e5e6c9492513
邱郁方 <yufang@qiuyufangde-MacBook-Pro.local>
2019-07-03 20:05:37 +0800

    week8-revise

--- homeworks/week8/hw2/script.js
+++ homeworks/week8/hw2/script.js
@@ -1,7 +1,9 @@
-// 建立讀取留言函式
+// function
+// 建立讀取留言
 function loadMessage() {
   const request = new XMLHttpRequest();
   const container = document.querySelector('.view_posts');
+  container.innerHTML = ''; // 清除內容再載入
   request.addEventListener('load', () => {
     if (request.status >= 200 && request.status < 400) {
       const getJson = JSON.parse(request.responseText);
@@ -29,26 +31,33 @@ function loadMessage() {
   request.send();
 }
 
-// 載入留言
-loadMessage();
-
-// 建立傳送留言函式
-function sendForm() {
+// post 留言
+function sendMessage() {
   const formData = `content=${document.getElementById('new_post_content').value}`;
   const xhr = new XMLHttpRequest();
   xhr.open('POST', 'https://lidemy-book-store.herokuapp.com/posts', true);
   xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
   xhr.send(formData);
+  // 清除留言框格內容
+  document.getElementById('new_post_content').value = '';
+  // 確認有資料回傳且request成功
+  xhr.onload = () => {
+    if (xhr.status >= 200 && xhr.status < 400) {
+      // 重新載入新增後的資料
+      loadMessage();
+    }
+  };
 }
 
-// 點擊按鈕時post
+// 開啟網頁時載入留言
+loadMessage();
+
+// 新增留言
 const btn = document.querySelector('#btn');
 btn.addEventListener('click', () => {
   if (document.getElementById('new_post_content').value === '') {
     alert('請輸入留言內容');
   } else {
-    sendForm();
-    document.location.href = './index.html';
-    loadMessage();
+    sendMessage();
   }
 });
